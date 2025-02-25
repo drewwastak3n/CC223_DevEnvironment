e35be7d2668df51cd789bc224c08adbb92205f6f
Nicholas Andrew Alcantara <146053170+drewwastak3n@users.noreply.github.com>
2025-02-25 19:35:28 +0800

    Delete CC223_WebApp/app.js

--- CC223_WebApp/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("fetchButton").addEventListener("click", () => {
-    fetch("http://localhost:3000/data")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("data").textContent = data.message;
-        })
-        .catch(error => console.error("Error fetching data:", error));
-});
